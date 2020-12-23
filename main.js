bc4c9705d8916b784c37b2364e950d8e3df2dfb7
Francesco Negri <francesconegri@gmail.com>
2020-12-23 22:10:02 +0100

    Import 20+20 website and update content
    Content imported from https://github.com/dhinus/20plus20 and then
    modified to reference the 20+21 edition.

---
+++ main.js
@@ -0,0 +1,327 @@
+
+const texts = {
+  whatIs20plus20Title: {
+    en: 'INFO',
+    pt: 'INFO'
+  },
+  whatIs20plus20Description: {
+    en: `<strong>WHAT IS 20+21 FEST?</strong>
+
+A small, unique music event. By music fans, for music fans.
+
+When we imagined 20+20 Fest, we wrote it was "a different night for different times". But we couldn't imagine how much different 2020 would turn out to be.
+
+Yet we believe that the original spirit of the festival &mdash; uniting people and artists from different countries &mdash; is now more important than ever. We want music to travel again, without borders.
+
+<strong>WHEN IS 20+21 FEST?</strong>
+
+The festival was originally scheduled for 20 March 2020, and was postponed due to the COVID-19 outbreak. A new date will be announced as soon as possible. We hope it will be in 2021, or we'll have to change the name again. :)
+
+<strong>LINKS</strong>
+
+<a href="https://www.facebook.com/20plus20/"><img class="social" src="img/fb.png" alt="Facebook"/></a> &nbsp;&nbsp;&nbsp; <a href="https://www.instagram.com/20plus20_fest/"><img class="social" src="img/ig.png" alt="Instagram"/></a>
+
+<strong>PLAYLIST</strong>
+
+<iframe src="https://open.spotify.com/embed/playlist/786BQbntCaSAEAflhgi04I" width="280" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+`,
+    pt: `<strong>O QUE É 20+21 FEST?</strong>
+
+Um evento musical pequeno e único. Por fãs de música, para fãs de música.
+
+Quando imaginámos o 20+20 Fest, escrevemos que iria ser "uma noite diferente para tempos diferentes". Mas não podíamos imaginar que 2020 teria sido tão diferente.
+
+Ainda acreditamos que o espírito original do festival &mdash; unir pessoas e artistas de diferentes países &mdash; é agora mais importante do que nunca. Queremos que a música viaje novamente, sem fronteiras.
+
+<strong>QUANDO É O 20+21 FEST?</strong>
+
+O festival estava originalmente marcado para 20 de março de 2020 e foi adiado devido ao surto do COVID-19. Uma nova data será anunciada assim que possível. Esperamos que seja em 2021, ou teremos que mudar novamente o nome. :)
+
+<strong>LINKS</strong>
+
+<a href="https://www.facebook.com/20plus20/"><img class="social" src="img/fb.png" alt="Facebook"/></a> &nbsp;&nbsp;&nbsp; <a href="https://www.instagram.com/20plus20_fest/"><img class="social" src="img/ig.png" alt="Instagram"/></a>
+
+<strong>PLAYLIST</strong>
+
+<iframe src="https://open.spotify.com/embed/playlist/786BQbntCaSAEAflhgi04I" width="280" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+`
+  },
+  lineUpTitle: {
+    en: `ARTISTS`,
+    pt: `ARTISTAS`
+  },
+  lineUpDescription: {
+    en: `<strong><a href="https://www.facebook.com/giardinidimiro/">GIARDINI DI MIRÒ</a> (Italy)</strong>
+
+<img src="img/gdm.jpg"/>
+
+Twenty years of career, more than a thousand concerts all over the world, Giardini di Mirò are a reference name in the Italian independent scene since the early 2000s. Their sound is a mix of psychedelia, post rock, electronic music, shoegaze, noise.
+
+<iframe src="https://open.spotify.com/embed/album/6Nu91SKkm3c3mtm6YLVBfo" width="280" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+<hr>
+
+<strong><a href="https://www.facebook.com/whalespt/">WHALES</a> (Portugal)</strong>
+
+<img src="img/whales.jpg"/>
+
+Whales started by winning one of the biggest band contests in Portugal “Festival Termómetro” as well as releasing their first single “Big Pulse Waves” who allowed them to play in festivals such as NOS Alive and Festival Bons Sons. In 2018 they released their debut album “Whales” from which they released singles such as “Ghost” that was nominated for low budget category in Berlin Music Video Awards and “Twerp” which debuted on the British Clash Magazine. With this album they toured around 7 countries all around Europe.
+
+<iframe src="https://open.spotify.com/embed/album/3PIohVfFBiPwziZYrSOsRv" width="280" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+<hr>
+
+<strong><a href="https://www.facebook.com/LaBaqMusic/">LABAQ</a> (Brazil / Italy)</strong>
+
+<img src="img/labaq.jpg"/>
+
+There are intimacies that pass over the flesh, like a translucent laser that directly touches the bone. Labaq achieves that effect with her songs: she is and is not, at the same time, singer-songwriter, indietronic icon, nova-bossa-nova, pop explorer, a songwriter as close to indie-folk as to r&b. In any case, the paulista is a celebrated artist that moves by a fine line of many things, to get to be herself.
+
+<iframe src="https://open.spotify.com/embed/album/47zxoaS3B8wEcN87eXIYn0" width="280" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+<hr>
+
+<strong><a href="https://www.facebook.com/littlefriendsongs/">LITTLE FRIEND</a> (UK / Portugal)</strong>
+
+<img src="img/little_friend.jpg"/>
+
+Little Friend is the alter ego of John Almeida, an English-born musician and writer based in Porto, Portugal. Having in previous years played and fronted punk and rock-influenced bands, he chose to give a different direction to Little Friend’s sound, focusing on influences that encompass a wide array of musical sources, from 60s and 70s folk and singer-songwriter pop, to film soundtracks and country.
+
+<iframe src="https://open.spotify.com/embed/album/3JqP1EcErBg4LnB60nL3BB" width="280" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+
+
+    `,
+    pt: `<strong><a href="https://www.facebook.com/giardinidimiro/">GIARDINI DI MIRÒ</a> (Itália)</strong>
+
+<img src="img/gdm.jpg"/>
+
+Vinte anos de carreira, mais de mil shows em todo o mundo, Giardini di Mirò são um nome de referência no cenário independente italiano desde o início dos anos 2000. O som deles é uma mistura de psicodelia, pós-rock, música eletrônica, shoegaze, ruído.
+
+<iframe src="https://open.spotify.com/embed/album/6Nu91SKkm3c3mtm6YLVBfo" width="280" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+<hr>
+
+<strong><a href="https://www.facebook.com/whalespt/">WHALES</a> (Portugal)</strong>
+
+<img src="img/whales.jpg"/>
+
+Começaram por vencer o festival termómetro em 2016, seguido do lançamentodo seu primeiro single “Big Pulse Waves”, com o qual pisaram palcos de festivais como o NOS Alive e o Festival Bons Sons. Em 2018 lançam o seu primeiro álbum “Whales”, do qual saíram singles como “Ghost”, nomeado para categoria Low Budget do Berlin Music Video Awards e “Twerp” que estreou na revista online britânica Clash Magazine. Com este álbum percorreram cerca de 7 países por toda a Europa.
+
+<iframe src="https://open.spotify.com/embed/album/3PIohVfFBiPwziZYrSOsRv" width="280" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+<hr>
+
+<strong><a href="https://www.facebook.com/LaBaqMusic/">LABAQ</a> (Brasil / Itália)</strong>
+
+<img src="img/labaq.jpg"/>
+
+Existem intimidades que passam por cima da carne, como um laser translúcido que toca diretamenteo osso. Labaq consegue esse efeito com suas canções: é e não é, ao mesmo tempo, cantautora, ícone do pop alternativo, exploradora pop, uma compositora tão perto do indie-folk como dos sons eletrônicos. Em todo caso, a paulista tem um talento que se move por uma linha fina de muitas coisas para conseguir ser ela mesma.
+
+<iframe src="https://open.spotify.com/embed/album/47zxoaS3B8wEcN87eXIYn0" width="280" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+<hr>
+
+<strong><a href="https://www.facebook.com/littlefriendsongs/">LITTLE FRIEND</a> (UK / Portugal)</strong>
+
+<img src="img/little_friend.jpg"/>
+
+Little Friend é o projecto de John Almeida, um músico e escritor inglês de ascendência portuguesa, baseado no Porto. Depois de vários anos envolvido em projectos criativos na sua cidade natal de Londres, e de uma passagem por Vancouver, para estudar Guionismo na Vancouver Film School, regressou a Portugal, onde cresceu, e começou a dar forma aos Little Friend.
+A música dos Little Friend tem influências que vão desde o folk ao indie, sendo que estas não limitam o seu som. A composição melódica e as letras introspectivas desempenham um papel muito importante no seu trabalho.
+
+<iframe src="https://open.spotify.com/embed/album/3JqP1EcErBg4LnB60nL3BB" width="280" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+
+
+`
+  },
+  infoTicketsTitle: {
+    en: `TICKETS & LOCATION`,
+    pt: `BILHETES & LOCALIZAÇÃO`
+  },
+  infoTicketsDescription: {
+    en: `<strong>TICKETS</strong>
+
+Not available yet.
+
+<hr>
+
+<strong>LOCATION</strong>
+
+<a href="https://www.facebook.com/auditorioccop/"><img src="img/auditorio.jpg"/></a>
+
+<strong><a href="https://www.facebook.com/auditorioccop/">Auditório CCOP</a></strong>
+
+Rua do Duque de Loulé 202
+4000-324 Porto, Portugal
+
+<a href="https://goo.gl/maps/S3XLcHbnCjubvDTg6"><img src="img/gmaps.png"/></a>
+
+
+`,
+    pt: `<strong>BILHETES</strong>
+
+Bilhetes ainda não disponíveis.
+
+<hr>
+
+<strong>LOCALIZAÇÃO</strong>
+
+<a href="https://www.facebook.com/auditorioccop/"><img src="img/auditorio.jpg"/></a>
+
+<strong><a href="https://www.facebook.com/auditorioccop/">Auditório CCOP</a></strong>
+
+Rua do Duque de Loulé 202
+4000-324 Porto, Portugal
+
+<a href="https://goo.gl/maps/S3XLcHbnCjubvDTg6"><img src="img/gmaps.png"/></a>
+
+`,
+  },
+  navBarMessage: {
+    en: '    +++    20+20 FEST IS POSTPONED AND BECOMES 20+21 FEST.    +++    THE DATE WILL BE ANNOUNCED AS SOON AS POSSIBLE.',
+    pt: '    +++    O 20-20 FEST FOI ADIADO E TORNOU-SE NO 20+21 FEST.    +++    A DATA SERÁ ANUNCIADA ASSIM QUE POSSÍVEL.'
+  },
+  navBarMessage2: {
+    en: '    +++    20+20 FEST IS POSTPONED AND BECOMES 20+21 FEST.    +++    THE DATE WILL BE ANNOUNCED AS SOON AS POSSIBLE.',
+    pt: '    +++    O 20-20 FEST FOI ADIADO E TORNOU-SE NO 20+21 FEST.    +++    A DATA SERÁ ANUNCIADA ASSIM QUE POSSÍVEL.'
+  },
+  toggleLanguage: {
+    en: '[en/pt]',
+    pt: '[en/pt]'
+  }
+}
+
+
+let userLanguage = navigator.language.slice(0, 2) === 'pt' ? 'pt' : 'en';
+
+document.getElementById('toggleLanguage').addEventListener('click', toggleLanguage);
+
+document.getElementById('mobileMenu').addEventListener('click', onMobileMenuClick);
+
+document.querySelectorAll('.logo').forEach(el => {
+  el.addEventListener('click', onLogoClick);
+});
+
+function onLogoClick() {
+  if (isMobile()) {
+    initializeMobile();
+  } else {
+    closeSections();
+    document.getElementById('logoVertical').className = 'hidden';
+    document.getElementById('logoHorizontal').className = '';
+  }
+}
+
+function isMobile() {
+  const md = new MobileDetect(window.navigator.userAgent);
+  return md.mobile() !== null;
+}
+
+const formatText = (text) => {
+  return `<span>${text.replace(/\n/g, '<br/>')}</span>`;
+}
+
+const populateTexts = () => {
+  const textIds = Object.keys(texts);
+
+  textIds.forEach(textId => {
+    document.getElementById(textId).innerHTML = formatText(texts[textId][userLanguage]);
+  });
+}
+
+function toggleLanguage() {
+  userLanguage = userLanguage === 'en' ? 'pt' : 'en';
+  populateTexts(userLanguage);
+}
+
+function closeSections() {
+  document.querySelectorAll('.section:not(.closed)').forEach(element => {
+    element.className = `${element.className} closed`;
+  });
+}
+
+function openSection(sectionId) {
+  // close every section
+  closeSections();
+
+  // open clicked section
+  document.getElementById(sectionId).className = document.getElementById(sectionId).className.replace('closed', '').trim();
+
+  window.openSectionId = sectionId;
+
+  // invert logo
+  if (isMobile()) {
+    // change menu icon
+    document.getElementById('icon-times').setAttribute('class', 'hidden');
+    document.getElementById('icon-bars').setAttribute('class', '');
+  } else {
+    document.getElementById('logoHorizontal').className = 'hidden';
+    document.getElementById('logoVertical').className = '';
+  }
+}
+
+function openWhatIs20plus20() {
+  openSection('whatIs20plus20');
+}
+function openLineUp() {
+  openSection('lineUp');
+}
+function openInfoTickets() {
+  openSection('infoTickets');
+}
+
+function onMobileMenuClick() {
+  if (document.getElementById('icon-bars').className.baseVal === 'hidden') {
+    // go back to last open section, if present
+    if (window.openSectionId) {
+      openSection(window.openSectionId);
+    } else {
+      initializeMobile();
+    }
+  } else {
+    // logo
+    document.getElementById('logoHorizontal').className = '';
+    document.getElementById('logoVertical').className = 'hidden';
+
+    // close every section
+    closeSections();
+
+    // show sections
+    document.getElementById('sections').className = document.getElementById('sections').className.replace(/hidden/g, '').trim();
+
+    // set "x" icon
+    document.getElementById('icon-times').setAttribute('class', '');
+    document.getElementById('icon-bars').setAttribute('class', 'hidden');
+  }
+}
+
+function initializeMobile() {
+  document.body.className = 'mobile';
+
+  window.openSectionId = null;
+
+  // menu
+  document.getElementById('icon-times').setAttribute('class', 'hidden');
+  document.getElementById('icon-bars').setAttribute('class', '');
+
+  // logo
+  document.getElementById('logoHorizontal').className = 'hidden';
+  document.getElementById('logoVertical').className = '';
+
+  // close every section
+  closeSections();
+
+  // hide sections
+  document.getElementById('sections').className = `${document.getElementById('sections').className} hidden`;
+}
+
+function main() {
+  if (isMobile()) {
+    initializeMobile();
+  }
+  populateTexts(userLanguage);
+}
+
+main();
